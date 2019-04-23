47ce2556455bfce5f507369ab84d61fb7a4106fe
Dion <dionvanderlinden@live.nl>
2019-04-23 12:24:57 +0200

    Inladen texture en opzetten scene

---
+++ 3DGraphics_Opdrachten/Week2_Minecraft/main.cpp
@@ -0,0 +1,270 @@
+#include <GL/freeglut.h>
+#include <cstdio>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <time.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+
+
+float lastFrameTime = 0;
+
+int width, height;
+GLuint textureId;
+
+
+struct Camera
+{
+	float posX = 0;
+	float posY = -4;
+	float rotX = 0;
+	float rotY = 0;
+} camera;
+
+bool keys[255];
+
+
+void drawCube()
+{
+	glBegin(GL_QUADS);
+	glColor3f(1, 0, 0);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(-1, 1, -1);
+
+	glColor3f(1, 1, 0);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+
+	glColor3f(0, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(1, -1, 1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 1, 0);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(1, 1, 0);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+	glEnd();
+}
+
+void drawTexCube()
+{
+	//glColor3f(1, 1, 1);
+
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, textureId);
+
+	glBegin(GL_QUADS);
+	
+
+	glTexCoord2f(0, 0); glVertex3f(-1, -1, -1);
+	glTexCoord2f(1, 0); glVertex3f(1, -1, -1);
+	glTexCoord2f(1, 1); glVertex3f(1, 1, -1);
+	glTexCoord2f(0, 1); glVertex3f(-1, 1, -1);
+
+	glTexCoord2f(0, 0); glVertex3f(-1, -1 , 1);
+	glTexCoord2f(0, 1); glVertex3f(1, -1, 1);
+	glTexCoord2f(1, 1); glVertex3f(1, 1, 1);
+	glTexCoord2f(1, 0); glVertex3f(-1, 1, 1);
+
+	glTexCoord2f(0, 0); glVertex3f(-1, -1, -1);
+	glTexCoord2f(0, 1); glVertex3f(-1, 1, -1);
+	glTexCoord2f(1, 1); glVertex3f(-1, 1, 1);
+	glTexCoord2f(1, 0); glVertex3f(-1, -1, 1);
+
+	glTexCoord2f(0, 0);  glVertex3f(1, -1, -1);
+	glTexCoord2f(0, 1); glVertex3f(1, 1, -1);
+	glTexCoord2f(1,1); glVertex3f(1, 1, 1);
+	glTexCoord2f(1, 0); glVertex3f(1, -1, 1);
+
+	glTexCoord2f(0, 0); glVertex3f(-1, -1, -1);
+	glTexCoord2f(0, 1); glVertex3f(1, -1, -1);
+	glTexCoord2f(1, 1); glVertex3f(1, -1, 1);
+	glTexCoord2f(1, 0); glVertex3f(-1, -1, 1);
+
+	glTexCoord2f(0, 0); glVertex3f(-1, 1, -1);
+	glTexCoord2f(0, 1); glVertex3f(1, 1, -1);
+	glTexCoord2f(1, 1); glVertex3f(1, 1, 1);
+	glTexCoord2f(1, 0); glVertex3f(-1, 1, 1);
+
+	glEnd();
+}
+void drawCube(int index)
+{
+
+}
+
+void display()
+{
+	glClearColor(0.6f, 0.6f, 1, 1);
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0f, (float)width / height, 0.1, 30);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glRotatef(camera.rotX, 1, 0, 0);
+	glRotatef(camera.rotY, 0, 1, 0);
+	glTranslatef(camera.posX, 0, camera.posY);
+
+
+	
+	glBegin(GL_QUADS);
+	//glColor3f(0.1f, 1.0f, 0.2f);
+	glVertex3f(-15, -1, -15);
+	glVertex3f(15, -1, -15);
+	glVertex3f(15, -1, 15);
+	glVertex3f(-15, -1, 15);
+	glEnd();
+
+	glPushMatrix();
+
+	glTranslatef(0, 0.0f, 0);
+	drawTexCube();
+	glPopMatrix();
+	/*for (int x = -10; x <= 10; x += 5)
+	{
+		for (int y = -10; y <= 10; y += 5)
+		{
+			glPushMatrix();
+			glTranslatef((float)x, 0.0f, (float)y);
+			drawCube();
+			glPopMatrix();
+		}
+	}*/
+
+	glutSwapBuffers();
+}
+
+void move(float angle, float fac)
+{
+	camera.posX += (float)cos((camera.rotY + angle) / 180 * M_PI) * fac;
+	camera.posY += (float)sin((camera.rotY + angle) / 180 * M_PI) * fac;
+}
+
+void idle()
+{
+	float frameTime = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
+	float deltaTime = frameTime - lastFrameTime;
+	lastFrameTime = frameTime;
+
+	const float speed = 3;
+	if (keys['a']) move(0, deltaTime*speed);
+	if (keys['d']) move(180, deltaTime*speed);
+	if (keys['w']) move(90, deltaTime*speed);
+	if (keys['s']) move(270, deltaTime*speed);
+
+	glutPostRedisplay();
+}
+
+bool justMovedMouse = false;
+void mousePassiveMotion(int x, int y)
+{
+	int dx = x - width / 2;
+	int dy = y - height / 2;
+	if ((dx != 0 || dy != 0) && abs(dx) < 400 && abs(dy) < 400 && !justMovedMouse)
+	{
+		camera.rotY += dx / 10.0f;
+		camera.rotX += dy / 10.0f;
+	}
+	if (!justMovedMouse)
+	{
+		glutWarpPointer(width / 2, height / 2);
+		justMovedMouse = true;
+	}
+	else
+		justMovedMouse = false;
+}
+
+void keyboard(unsigned char key, int, int)
+{
+	if (key == 27)
+		exit(0);
+	keys[key] = true;
+}
+
+void keyboardUp(unsigned char key, int, int)
+{
+	keys[key] = false;
+}
+void initGraphics()
+{
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_ALPHA_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glGenTextures(1, &textureId);
+	glBindTexture(GL_TEXTURE_2D, textureId);
+	
+	/*unsigned char data[32 * 32 * 4];
+	for (int i = 0; i < 32 * 32 * 4; i++)
+	{
+		data[i] = rand() % 256;
+	}*/
+
+	int w, h, bpp;
+	stbi_set_flip_vertically_on_load(1);
+
+	unsigned char* imgData = stbi_load("mike_wazowski.jpg", &w, &h, &bpp, 4);
+	glTexImage2D(GL_TEXTURE_2D, 
+				0, 
+				GL_RGBA,
+				w,
+				h,
+				0,
+				GL_RGBA,
+				GL_UNSIGNED_BYTE,
+				imgData);
+	stbi_image_free(imgData);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+}
+int main(int argc, char* argv[])
+{
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(1920, 1080);
+	glutInit(&argc, argv);
+	glutCreateWindow("Hello World");
+
+	memset(keys, 0, sizeof(keys));
+	srand(time(NULL));
+	
+
+	glutIdleFunc(idle);
+	glutDisplayFunc(display);
+	glutReshapeFunc([](int w, int h) { width = w; height = h; glViewport(0, 0, w, h); });
+	glutKeyboardFunc(keyboard);
+	glutKeyboardUpFunc(keyboardUp);
+	glutPassiveMotionFunc(mousePassiveMotion);
+
+	glutWarpPointer(width / 2, height / 2);
+	initGraphics();
+	
+	glutMainLoop();
+
+
+	return 0;
+}
